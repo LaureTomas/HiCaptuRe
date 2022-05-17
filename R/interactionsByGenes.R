e53a2422742ecbf0654c777af32b1b8d5bb0de12
Laureano Tomás-Daza <lauretomas@gmail.com>
2022-05-17 09:49:41 +0200

    Not using an annotation file and specific column anymore

--- R/interactionsByGenes.R
+++ R/interactionsByGenes.R
@@ -1,42 +1,31 @@
-#' Computes the intersect between a vector of genes and interactions
+#' Filters GenomicInteractions object by baits
 #'
-#' This function computes the intersect between a given vector of genes and a GenomicInteractions objects using an annotaion file
+#' his function filters a GenomicInteractions object from load_interactions by a set of given bait(s)
 #'
 #' @param interactions a GenomicInteractions object from \code{\link{load_interactions}}
-#' @param genes character vector containing genes
-#' @param annotation full path to annotation file which MUST include header as follows: chr, start, end, and at least a colum with genes_annot
-#' @param genes_annot column name of annation file with the annotation in the same format as the genes
+#' @param baits character vector containing bait names
 #'
-#' @return GenomicInteractions object, subset of the original based on the give genes
+#' @return GenomicInteractions object, subset of the original based on the give baits
 #'
 #' @importFrom stringr str_split
-#' @importFrom IRanges subsetByOverlaps
-#' @importFrom GenomicRanges makeGRangesFromDataFrame
-#' @importFrom data.table fread
 #'
 #' @export
-interactionsByGenes <- function(interactions, genes, annotation, genes_annot)
+interactionsByBaits <- function(interactions, baits)
 {
-  message("Remember: Annotation must be, at least, chr start end genes_annot\n MUST INCLUDE HEADER")
-  annot <- data.table::fread(annotation, header=T)
-  if(any(colnames(annot) %in% "V1"))
+  one <- sapply(stringr::str_split(as.character(interactions$gene_I),","), function(x) any(x %in% baits))
+  two <- sapply(stringr::str_split(as.character(interactions$gene_II),","), function(x) any(x %in% baits))
+  interactions_baits <- interactions[one | two]
+
+  if (length(interactions_baits) == 0)
   {
-    stop("Annotation file MUST include header")
+    message("There is no interaction with the given bait(s)")
+    message("Could Interactions not be annotated with the same bait nomenclature?")
+
   }
   else
   {
-    mat <- sapply(stringr::str_split(as.character(annot[[genes_annot]]),","), function(x) any(x %in% genes))
-    matGR <- GenomicRanges::makeGRangesFromDataFrame(annot[mat,], seqnames.field = "chr",start.field = "start",end.field = "end")
-    message(paste("There are",length(matGR),"HindIII fragments with genes"))
-    gi <- IRanges::subsetByOverlaps(interactions,matGR)
-    if (length(gi) == 0)
-    {
-      message("There is no interaction with the given gene(s)")
-    }
-    else
-    {
-      message(paste("There are",length(gi), "interaction with the given gene(s)"))
-    }
-    return(gi)
+    message(paste("There are",length(interactions_baits), "interaction with the given bait(s)"))
   }
+  return(gi)
+
 }
