642f3262a0a387b321427d42f309f7a4f8d26247
Laureano Tomás-Daza <lauretomas@gmail.com>
2022-05-17 09:27:56 +0200

    Changing how to annotate each end based on overlap, and how to classify interactions

--- R/integrate_regions.R
+++ R/integrate_regions.R
@@ -1,6 +1,6 @@
 #' Filters GenomicInteractions object by integrating a regions file
 #'
-#' This function filters a GenomicInteractions object from load_interactions by integrating regions from a external regions file, and classifies the interactions by type of interactions based on if the right, left or both end overlap with a region
+#' This function filters a GenomicInteractions object from load_interactions by integrating regions and classifies the interactions by type of interactions based on if the right, left or both end overlap with a region
 #'
 #' @param interactions GenomicInteractions object from \code{\link{load_interactions}}
 #' @param regions full path to regions file (bed format) or a GRanges object
@@ -9,17 +9,16 @@
 #' @param end column name of end positions
 #' @param invert T/F if need those interactions that do NOT overlaps with any regions
 #'
-#' @return GenomicInteractions object filtered by regions with 'int' column containing type of interactions
+#' @return GenomicInteractions object filtered by regions, by default with additional columns regarding overlap on each node. If invert=T no additional columns.
 #'
 #' @importFrom magrittr `%>%`
-#' @importFrom dplyr as_tibble group_by summarise n
 #' @importFrom GenomicInteractions anchorOne anchorTwo
-#' @importFrom GenomicRanges makeGRangesFromDataFrame findOverlaps
-#' @importFrom IRanges subsetByOverlaps
+#' @importFrom GenomicRanges makeGRangesFromDataFrame
+#' @importFrom IRanges subsetByOverlaps overlapsAny countOverlaps
 #' @importFrom data.table fread
 #'
 #' @export
-integrate_regions <- function(interactions,regions,chr=NULL,start=NULL,end=NULL, invert=F)
+interactionsByRegions <- function(interactions,regions,chr=NULL,start=NULL,end=NULL, invert=F)
 {
   ## Setting pipe operator from magrittr package
   `%>%` <- magrittr::`%>%`
@@ -53,7 +52,7 @@ integrate_regions <- function(interactions,regions,chr=NULL,start=NULL,end=NULL,
   {
     interactions_regions <- unique(IRanges::subsetByOverlaps(interactions, regionsGR, invert = invert))
     message(paste("Integration with regions results in",length(interactions_regions),"interactions that do not overlap with any given region"))
-
+    annotate_POEuce(interactions_regions)
   }
   else
   {
@@ -65,66 +64,22 @@ integrate_regions <- function(interactions,regions,chr=NULL,start=NULL,end=NULL,
     {
 
       ## Annotating and calculating overlaps in anchor one
-      a <- dplyr::as_tibble(GenomicRanges::findOverlaps(GenomicInteractions::anchorOne(interactions_regions), regionsGR)) %>% dplyr::group_by(queryHits) %>% dplyr::summarise(n_overlap=dplyr::n())
-      interactions_regions$overlap_I <- F
-      interactions_regions$overlap_I[a$queryHits] <- T
-      interactions_regions$n_overlap_I <- 0
-      interactions_regions$n_overlap_I[a$queryHits] <- a$n_overlap
-
-      ## TODO use countoverlaps instead of findoverlaps
+      interactions_regions$overlap_I <- IRanges::overlapsAny(anchorOne(interactions_regions), regionsGR)
+      interactions_regions$n_overlap_I <- IRanges::countOverlaps(anchorOne(interactions_regions), regionsGR)
 
       ## Annotating and calculating overlaps in anchor two
-      b <- dplyr::as_tibble(GenomicRanges::findOverlaps(GenomicInteractions::anchorTwo(interactions_regions), regionsGR)) %>% dplyr::group_by(queryHits) %>% dplyr::summarise(n_overlap=dplyr::n())
-      interactions_regions$overlap_II <- F
-      interactions_regions$overlap_II[b$queryHits] <- T
-      interactions_regions$n_overlap_II <- 0
-      interactions_regions$n_overlap_II[b$queryHits] <- b$n_overlap
+      interactions_regions$overlap_II <- IRanges::overlapsAny(anchorTwo(interactions_regions), regionsGR)
+      interactions_regions$n_overlap_II <- IRanges::countOverlaps(anchorTwo(interactions_regions), regionsGR)
 
 
+      annotate_POEuce(interactions_regions)
       ## Annotating type of interactions depending of overlap with regions
-      interactions_regions$int <- ifelse(interactions_regions$overlap_I & interactions_regions$overlap_II &
-                                           (interactions_regions$gene_I == "." | interactions_regions$gene_II == ".") &
-                                           (interactions_regions$gene_I != "uce" & interactions_regions$gene_II != "uce"), "P_OE_II",
-                                         ifelse(interactions_regions$overlap_I & interactions_regions$overlap_II &
-                                                  (interactions_regions$gene_I == "." | interactions_regions$gene_II == ".") &
-                                                  (interactions_regions$gene_I == "uce" | interactions_regions$gene_II == "uce"), "uce_OE_II",
-                                                ifelse(interactions_regions$overlap_I & interactions_regions$overlap_II &
-                                                         !interactions_regions$gene_I %in% c(".","uce") & !interactions_regions$gene_II %in% c(".","uce"),"P_P_II",
-                                                       ifelse(interactions_regions$overlap_I & interactions_regions$overlap_II & interactions_regions$gene_I != "." & interactions_regions$gene_II != "." &
-                                                                (interactions_regions$gene_I == "uce" | interactions_regions$gene_II == "uce") &
-                                                                !(interactions_regions$gene_I == "uce" & interactions_regions$gene_II == "uce"),"P_uce_II",
-                                                              ifelse((interactions_regions$overlap_I | interactions_regions$overlap_II) & !(interactions_regions$overlap_I == interactions_regions$overlap_II) &
-                                                                       !interactions_regions$gene_I %in% c(".","uce") & !interactions_regions$gene_II %in% c(".","uce"),"P_P_I",
-                                                                     ifelse((interactions_regions$overlap_I & !interactions_regions$overlap_II &
-                                                                               !interactions_regions$gene_I %in% c(".","uce") & interactions_regions$gene_II == ".") |
-                                                                              (interactions_regions$overlap_II & !interactions_regions$overlap_I &
-                                                                                 !interactions_regions$gene_II %in% c(".","uce") & interactions_regions$gene_I == "."),"P_OE_I",
-                                                                            ifelse((interactions_regions$overlap_I & !interactions_regions$overlap_II &
-                                                                                      !interactions_regions$gene_II %in% c(".","uce") & interactions_regions$gene_I == ".") |
-                                                                                     (interactions_regions$overlap_II & !interactions_regions$overlap_I &
-                                                                                        !interactions_regions$gene_I %in% c(".","uce") & interactions_regions$gene_II == "."),"OE_P_I",
-                                                                                   ifelse((interactions_regions$overlap_I & !interactions_regions$overlap_II &
-                                                                                             interactions_regions$gene_I == "uce" & !interactions_regions$gene_II %in% c(".","uce")) |
-                                                                                            (interactions_regions$overlap_II & !interactions_regions$overlap_I &
-                                                                                               interactions_regions$gene_II == "uce" & !interactions_regions$gene_I %in% c(".","uce")),"uce_P_I",
-                                                                                          ifelse((interactions_regions$overlap_I & !interactions_regions$overlap_II &
-                                                                                                    interactions_regions$gene_II == "uce" & !interactions_regions$gene_I %in% c(".","uce")) |
-                                                                                                   (interactions_regions$overlap_II & !interactions_regions$overlap_I &
-                                                                                                      interactions_regions$gene_I == "uce" & !interactions_regions$gene_II %in% c(".","uce")),"P_uce_I",
-                                                                                                 ifelse((interactions_regions$overlap_I & !interactions_regions$overlap_II &
-                                                                                                           interactions_regions$gene_I == "uce" & interactions_regions$gene_II == ".") |
-                                                                                                          (interactions_regions$overlap_II & !interactions_regions$overlap_I &
-                                                                                                             interactions_regions$gene_II == "uce" & interactions_regions$gene_I == "."),"uce_OE_I",
-                                                                                                        ifelse((interactions_regions$overlap_I & !interactions_regions$overlap_II &
-                                                                                                                  interactions_regions$gene_I == "." & interactions_regions$gene_II == "uce") |
-                                                                                                                 (interactions_regions$overlap_II & !interactions_regions$overlap_I &
-                                                                                                                    interactions_regions$gene_II == "." & interactions_regions$gene_I == "uce"),"OE_uce_I",
-                                                                                                               ifelse((interactions_regions$overlap_I | interactions_regions$overlap_II) & !(interactions_regions$overlap_I == interactions_regions$overlap_II) &
-                                                                                                                        interactions_regions$gene_I=="uce" & interactions_regions$gene_II=="uce","uce_uce_I",
-                                                                                                                      ifelse(interactions_regions$overlap_I & interactions_regions$overlap_II &
-                                                                                                                               interactions_regions$gene_I=="uce" & interactions_regions$gene_II=="uce","uce_uce_II",NA)))))))))))))
-
-
+      interactions_regions$int <- ifelse(interactions_regions$overlap_I & interactions_regions$overlap_II,
+                                         paste(GenomicInteractions::anchorOne(interactions_regions)$node.class,GenomicInteractions::anchorTwo(interactions_regions)$node.class,"II", sep = "_"),
+                                         ifelse(interactions_regions$overlap_I & !interactions_regions$overlap_II,
+                                                paste(GenomicInteractions::anchorOne(interactions_regions)$node.class,GenomicInteractions::anchorTwo(interactions_regions)$node.class,"I", sep = "_"),
+                                                ifelse(!interactions_regions$overlap_I & interactions_regions$overlap_II,
+                                                       paste(GenomicInteractions::anchorTwo(interactions_regions)$node.class,GenomicInteractions::anchorOne(interactions_regions)$node.class,"I", sep = "_"),NA)))
 
     }
   }
