8a009baa0d66d84f7e382e486d79a1088f6784e6
Laureano Tomás-Daza <lauretomas@gmail.com>
2022-05-19 08:46:25 +0200

    adding ggplot2 dependencies

--- R/plot_distance_summary.R
+++ R/plot_distance_summary.R
@@ -9,7 +9,7 @@
 #'
 #' @return list with the plots
 #'
-#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 ggplot aes geom_bar labs theme_minimal theme element_text
 #'
 #' @export
 plot_distance_summary <- function(distances, type_of_value="absolute")
@@ -22,23 +22,23 @@ plot_distance_summary <- function(distances, type_of_value="absolute")
       distances1 <- distances[!(distances$int == "Total"),]
       distances1 <- distances1[!(distances1$breaks == "total_per_int"),]
 
-      g <- ggplot2::ggplot(distances1, aes(x=int, y=value, fill=breaks)) +
+      g <- ggplot2::ggplot(distances1, ggplot2::aes(x=int, y=value, fill=breaks)) +
         ggplot2::geom_bar(width = 1, size = 1, stat = "identity",position="dodge") +
         ggplot2::labs(x="Type of interactions",y="Number of interactions", fill="Distances",
              title=paste0("Number of interactions of ",unique(distances1$sample)," by type of interaction"),
-             subtitle = "Absolute Values") + theme(axis.text.x = element_text(angle = 70, hjust = 1)) +
+             subtitle = "Absolute Values") + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 70, hjust = 1)) +
         ggplot2::theme_minimal()
 
       plots[["int_dist"]] <- g
 
       distancesT <- distances[distances$int == "Total",]
 
-      g <- ggplot2::ggplot(distancesT, aes(x=breaks, y=value, fill=breaks)) +
+      g <- ggplot2::ggplot(distancesT, ggplot2::ggplot2::aes(x=breaks, y=value, fill=breaks)) +
         ggplot2::geom_bar(width = 1, size = 1, stat = "identity",position="dodge") +
         ggplot2::labs(x="Distances",y="Number of interactions", fill="Distances",
              title = paste0("Total Number of interactions of ",unique(distances1$sample)),
              subtitle = "Absolute Values") +
-        ggplot2::theme_minimal() + ggplot2::theme(axis.text.x = element_text(angle = 70, hjust = 1))
+        ggplot2::theme_minimal() + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 70, hjust = 1))
 
       plots[["total_dist"]] <- g
 
@@ -50,12 +50,12 @@ plot_distance_summary <- function(distances, type_of_value="absolute")
       distances1 <- distances[!(distances$int == "Total"),]
       distances1 <- distances1[!(distances1$breaks == "total_per_int"),]
 
-      g <- ggplot2::ggplot(distances1, aes(x=int, y=(value/total_per_int)*100, fill=breaks)) +
+      g <- ggplot2::ggplot(distances1, ggplot2::aes(x=int, y=(value/total_per_int)*100, fill=breaks)) +
         ggplot2::geom_bar(width = 1, size = 1, stat = "identity",position="dodge") +
         ggplot2::labs(x="Type of interactions",y="% of interactions", fill="Distances",
              title=paste0("% of interactions of ",unique(distances1$sample)," by type of interaction"),
              subtitle = "Normalized by the total number of interaction of each type") +
-        ggplot2::theme_minimal() + ggplot2::theme(axis.text.x = element_text(angle = 70, hjust = 1))
+        ggplot2::theme_minimal() + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 70, hjust = 1))
 
       plots[["int_dist_norm_int"]] <- g
 
@@ -68,23 +68,23 @@ plot_distance_summary <- function(distances, type_of_value="absolute")
       distances1 <- distances[!(distances$int == "Total"),]
       distances1 <- distances1[!(distances1$breaks == "total_per_int"),]
 
-      g <- ggplot2::ggplot(distances1, aes(x=int, y=(value/ibed)*100, fill=breaks)) +
+      g <- ggplot2::ggplot(distances1, ggplot2::aes(x=int, y=(value/ibed)*100, fill=breaks)) +
         ggplot2::geom_bar(width = 1, size = 1, stat = "identity",position="dodge") +
         ggplot2::labs(x="Type of interactions",y="% of interactions", fill="Distances",
              title=paste0("% of interactions of ",unique(distances1$sample)," by type of interaction"),
              subtitle = "Normalized by the Total number of interactions") +
-        ggplot2::theme_minimal() + ggplot2::theme(axis.text.x = element_text(angle = 70, hjust = 1))
+        ggplot2::theme_minimal() + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 70, hjust = 1))
 
       plots[["int_dist_norm_total"]] <- g
 
       distancesT <- distances[distances$int == "Total",]
 
-      g <- ggplot2::ggplot(distancesT, aes(x=breaks, y=(value/ibed)*100, fill=breaks)) +
+      g <- ggplot2::ggplot(distancesT, ggplot2::aes(x=breaks, y=(value/ibed)*100, fill=breaks)) +
         ggplot2::geom_bar(width = 1, size = 1, stat = "identity",position="dodge") +
         ggplot2::labs(x="Distances",y="Number of interactions", fill="Distances",
                       title = paste0("Total Number of interactions of ",unique(distances1$sample)),
                       subtitle = "Absolute Values") +
-        ggplot2::theme_minimal() + ggplot2::theme(axis.text.x = element_text(angle = 70, hjust = 1))
+        ggplot2::theme_minimal() + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 70, hjust = 1))
 
       plots[["total_dist_norm_total"]] <- g
 
