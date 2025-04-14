aa36bb59243675226fed64384d579ccfa8ddc68d
Laureano Tomás-Daza <lauretomas@gmail.com>
2025-04-14 09:55:09 +0200

    Edit documentation and adding for annotate_BOE Check first dataframe annotation Change call of elementMetadata and simplify some column update. Hardcoded columns sort

--- R/annotate_interactions.R
+++ R/annotate_interactions.R
@@ -1,14 +1,14 @@
-#' Annotates GenomicInteractions object from load_interactions
+#' Annotates HiCaptuRe object from load_interactions
 #'
-#' This function annotate a GenomicInteractions object from load_interactions, based on a given annotations file
+#' This function annotate a HiCaptuRe object from load_interactions, based on a given annotations file
 #'
-#' @param interactions GenomicInteractions object from \code{\link{load_interactions}}
-#' @param annotation full path to annotations file or a dataframe with 5 colums: chr, start, end, fragmentID, annotation
+#' @param interactions HiCaptuRe object from \code{\link{load_interactions}}
+#' @param annotation full path to annotations file or a dataframe with 5 columns: chr, start, end, fragmentID, annotation
 #' @param ... additional parameters for fread
 #'
 #' @return HiCaptuRe object annotated, with columns bait_1 and bait_2 substituted based on the given annotation
 #'
-#' @importFrom GenomicInteractions annotateInteractions anchorOne anchorTwo resetAnnotations annotateRegions
+#' @importFrom GenomicInteractions annotateInteractions anchorOne anchorTwo resetAnnotations
 #' @importFrom GenomicRanges makeGRangesFromDataFrame split
 #' @importFrom data.table fread
 #' @importFrom S4Vectors elementMetadata
@@ -16,18 +16,18 @@
 #'
 #' @export
 annotate_interactions <- function(interactions, annotation, ...) {
-  if (methods::is(annotation, "character")) {
+  if (any(class(annotation) == "data.frame")) {
+    annotation_file <- deparse(substitute(annotation))
+  } else if (methods::is(annotation, "character")) {
     annotation_file <- normalizePath(annotation)
     ## Reading annotation file
     annotation <- data.table::fread(annotation, stringsAsFactors = F, ...)
   }
-  if (any(class(annotation) == "data.frame")) {
-    annotation_file <- deparse(substitute(annotation))
 
     ## Checking if file has 5 columns
     if (ncol(annotation) != 5) {
-      stop("File has not 5 columns \nAnnotation must be chr start end fragmentID annotation")
-    } else {
+      stop("Annotation file must have exactly 5 columns: chr, start, end, fragmentID, annotation")
+      } else {
       ## Making a GRanges from the annotation and setting the NA as non-annotated
       cn <- colnames(annotation)
       annotation[[cn[5]]][is.na(annotation[[cn[5]]])] <- "non-annotated"
@@ -39,12 +39,12 @@ annotate_interactions <- function(interactions, annotation, ...) {
 
       suppressMessages(GenomicInteractions::annotateInteractions(interactions, annotation.features))
 
-      interactions@elementMetadata[, "bait_1"] <- unlist(GenomicInteractions::anchorOne(interactions)@elementMetadata[, "annot.id"])
-      interactions@elementMetadata[, "bait_2"] <- unlist(GenomicInteractions::anchorTwo(interactions)@elementMetadata[, "annot.id"])
+      interactions$bait_1 <- unlist(S4Vectors::elementMetadata(GenomicInteractions::anchorOne(interactions))[, "annot.id"])
+      interactions$bait_2 <- unlist(S4Vectors::elementMetadata(GenomicInteractions::anchorTwo(interactions))[, "annot.id"])
 
       ## When using a annotation file with only baits the OE get a NA so we have to change that
-      interactions@elementMetadata[is.na(interactions@elementMetadata[, "bait_2"]), "bait_2"] <- "."
-      interactions@elementMetadata[is.na(interactions@elementMetadata[, "bait_1"]), "bait_1"] <- "."
+      interactions$bait_1[is.na(interactions$bait_1)] <- "."
+      interactions$bait_2[is.na(interactions$bait_2)] <- "."
 
       interactions <- annotate_BOE(interactions)
 
@@ -56,7 +56,7 @@ annotate_interactions <- function(interactions, annotation, ...) {
       interactions@anchor1[cond] <- a2
       interactions@anchor2[cond] <- a1
 
-      cols <- sort(grep("_", colnames(S4Vectors::elementMetadata(interactions[cond]))[1:7], value = T))
+      cols <- c("bait_1", "bait_2", "ID_1", "ID_2")
       S4Vectors::elementMetadata(interactions[cond])[cols] <- S4Vectors::elementMetadata(interactions[cond])[cols[c(rbind(seq(2, length(cols), 2), seq(1, length(cols), 2)))]]
 
       interactions <- annotate_BOE(interactions)
@@ -68,15 +68,26 @@ annotate_interactions <- function(interactions, annotation, ...) {
 
       return(interactions)
     }
-  }
 }
 
 
+#' Annotates interactions into B_B or B_OE
+#'
+#' This function annotate a HiCaptuRe object based on the presence or not of annotation in a given restriction fragment
+#'
+#' @param interactions HiCaptuRe object from \code{\link{load_interactions}}
+#'
+#' @return HiCaptuRe object with the column int updated
+#'
+#' @importFrom GenomicInteractions annotateInteractions anchorOne anchorTwo resetAnnotations annotateRegions
+#' @importFrom GenomicRanges split
+#' @importFrom S4Vectors elementMetadata
+#' @keywords internal
 annotate_BOE <- function(interactions) {
   a1 <- GenomicInteractions::anchorOne(interactions)
-  a1$bait_1 <- interactions@elementMetadata[, "bait_1"]
+  a1$bait_1 <- S4Vectors::elementMetadata(interactions)[, "bait_1"]
   a2 <- GenomicInteractions::anchorTwo(interactions)
-  a2$bait_1 <- interactions@elementMetadata[, "bait_2"]
+  a2$bait_1 <- S4Vectors::elementMetadata(interactions)[, "bait_2"]
 
   a2$bait_1[is.na(a2$bait_1)] <- "."
 
