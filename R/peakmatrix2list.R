45c9d12f7f661e7744397756aa23f081f162e33e
Laureano Tomás-Daza <lauretomas@gmail.com>
2025-04-17 16:35:34 +0200

    styleR

--- R/peakmatrix2list.R
+++ R/peakmatrix2list.R
@@ -12,24 +12,23 @@
 #' @export
 peakmatrix2list <- function(peakmatrix, cutoff = 5) {
   parameters <- getParameters(peakmatrix)
-  format <-  parameters$load["format"]
+  format <- parameters$load["format"]
   if (tolower(format) != "peakmatrix") {
-    stop(paste0("Input is not a peakmatrix, it is a ",format))
-  } else if (!is.null(parameters$peakmatrix2list)){
+    stop(paste0("Input is not a peakmatrix, it is a ", format))
+  } else if (!is.null(parameters$peakmatrix2list)) {
     stop("The input object already appears to have been processed by 'peakmatrix2list()', which means it contains interactions for a single sample only. Please provide an unprocessed 'peakmatrix' object.")
   }
 
   m <- GenomicRanges::mcols(peakmatrix)
   CS <- grep("CS_", names(m))
-  if (is.na(CS))
-  {
+  if (is.na(CS)) {
     stop("No CS_* columns found in the provided HiCaptuRe object.")
   }
 
   int_list <- lapply(CS, function(x) {
     sub_int <- peakmatrix[m[, x] >= cutoff, which(!1:ncol(m) %in% CS[CS != x])]
     names(GenomicRanges::mcols(sub_int))[6] <- "CS"
-    parameters$peakmatrix2list <- c(split_by=names(m)[x])
+    parameters$peakmatrix2list <- c(split_by = names(m)[x])
     setParameters(sub_int) <- parameters
     sub_int
   })
