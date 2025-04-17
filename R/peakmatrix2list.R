3164c769197103c7256b2ec17239bbc19b309929
Laureano Tomás-Daza <lauretomas@gmail.com>
2025-04-17 16:35:00 +0200

    update documentation, include more errors, update the parameters, and subset including extra metadata columns

--- R/peakmatrix2list.R
+++ R/peakmatrix2list.R
@@ -3,28 +3,34 @@
 #' This function creates a list of HiCaptuRe objects separating all the samples included on a peakmatrix.
 #'
 #' @param peakmatrix HiCaptuRe object of a peakmatrix
-#' @param cutoff Chicago score cutoff to filter interactions
+#' @param cutoff Chicago score cut-off to filter interactions
 #'
-#' @return list with HiCaptuRe objects
+#' @return A named list of \code{HiCaptuRe} objects, each corresponding to a sample (inferred from CS_* columns in the peakmatrix).
 #'
 #' @importFrom GenomicRanges mcols
 #'
 #' @export
 peakmatrix2list <- function(peakmatrix, cutoff = 5) {
-  type <- peakmatrix@parameters$load["type"]
-  if (type != "peakmatrix") {
-    stop("Input is not a peakmatrix")
+  parameters <- getParameters(peakmatrix)
+  format <-  parameters$load["format"]
+  if (tolower(format) != "peakmatrix") {
+    stop(paste0("Input is not a peakmatrix, it is a ",format))
+  } else if (!is.null(parameters$peakmatrix2list)){
+    stop("The input object already appears to have been processed by 'peakmatrix2list()', which means it contains interactions for a single sample only. Please provide an unprocessed 'peakmatrix' object.")
   }
 
-  peakmatrix@parameters$load["type"] <- "ibed"
   m <- GenomicRanges::mcols(peakmatrix)
-  initial <- grep("CS_", names(m))[1] - 1
   CS <- grep("CS_", names(m))
-  final <- which(names(m) %in% c("counts", "int", "distance"))
+  if (is.na(CS))
+  {
+    stop("No CS_* columns found in the provided HiCaptuRe object.")
+  }
 
   int_list <- lapply(CS, function(x) {
-    sub_int <- peakmatrix[m[, x] >= cutoff, c(1:initial, x, final)]
-    names(GenomicRanges::mcols(sub_int))[initial + 1] <- "CS"
+    sub_int <- peakmatrix[m[, x] >= cutoff, which(!1:ncol(m) %in% CS[CS != x])]
+    names(GenomicRanges::mcols(sub_int))[6] <- "CS"
+    parameters$peakmatrix2list <- c(split_by=names(m)[x])
+    setParameters(sub_int) <- parameters
     sub_int
   })
 
