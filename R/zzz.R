05584f3a42ace7f16701aadab708eba5f4094a2e
Laureano Tomás-Daza <lauretomas@gmail.com>
2025-04-22 15:41:07 +0200

    add variables

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
   utils::globalVariables(c(
     ".SD", "B.id", "OE.id", "node.class", "ID", "ID_1", "ID_2", "annot", "bait_1", "bait_2", "breaks", "chr_1", "chr_2", "distance",
     "end1", "end2", "end_1", "end_2", "fragmentID", "int", "regionID", "seqnames1", "seqnames2",
-    "start1", "start2", "start_1", "start_2", "total_per_int", "value", "regionID_1", "regionID_2", "name", "score_names"
+    "start1", "start2", "start_1", "start_2", "total_per_int", "value", "regionID_1", "regionID_2", "name", "score_names","..flip"
   ))
   digest_genome <<- memoise::memoise(digest_genome)
 }
