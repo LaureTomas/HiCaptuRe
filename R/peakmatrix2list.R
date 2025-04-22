eb90d16164b0ba27f684506c5c67d6937a22c2bb
Laureano Tomás-Daza <lauretomas@gmail.com>
2025-04-22 15:40:59 +0200

    correct usage setparameters

--- R/peakmatrix2list.R
+++ R/peakmatrix2list.R
@@ -29,7 +29,7 @@ peakmatrix2list <- function(peakmatrix, cutoff = 5) {
     sub_int <- peakmatrix[m[, x] >= cutoff, which(!1:ncol(m) %in% CS[CS != x])]
     names(GenomicRanges::mcols(sub_int))[6] <- "CS"
     parameters$peakmatrix2list <- c(split_by = names(m)[x])
-    setParameters(sub_int) <- parameters
+    sub_int <- setParameters(sub_int, parameters)
     sub_int
   })
 
