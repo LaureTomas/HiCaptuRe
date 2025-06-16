71d28e86ffbd4090941fa5bfb5f0fbfa31298a7e
Laureano Tomás-Daza <lauretomas@gmail.com>
2025-06-16 16:19:47 +0200

    update docs

--- R/getters.R
+++ R/getters.R
@@ -1,4 +1,4 @@
-#' Functions to access data held in a HiCaptuRe object.
+#' Interanl functions to access data held in a HiCaptuRe object.
 #'
 #' Use these functions to access data stored in each of the slots of a
 #' HiCapture object.
